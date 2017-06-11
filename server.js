2fd9b47f9f3366874da7383d3509b9689211bd44
German Hernandez <germanhernandez.dr@gmail.com>
2017-06-11 12:06:27 +0100

    change express route

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ if(process.env.NODE_ENV !== 'production') {
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
